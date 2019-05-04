6a5311b9b42f96640d629fd6dfaf2448c1c41878
Paulo Henrique <gladgamer01@gmail.com>
2019-05-04 04:20:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
-const http = require("http").createServer();
-
-const io = require("socket.io")(http);
-
 var server = app.listen(80,()=>{
   console.log("running on localhost:80");
 });
+
+const io = require("socket.io")(server);
 
 app.use(express.static('public'));
 
@@ -19,8 +16,4 @@ io.on("connection",(socket)=> {
   socket.on("game",(data)=>{
     io.emit('game',data);
   })
-});
-
-http.listen(port,()=>{
-  console.log("server running!");
 });
