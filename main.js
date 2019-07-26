a962d3d5857c0f226c7cabcee5a74ae3aeb0b285
Paulo Henrique <gladgamer01@gmail.com>
2019-07-26 13:37:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,80 +1,64 @@
-const express = require('express');
-const app = express();
-
-function getRandomArrayElement(arr){
-    //Minimum value is set to 0 because array indexes start at 0.
-    var min = 0;
-    //Get the maximum value my getting the size of the
-    //array and subtracting by 1.
-    var max = (arr.length - 1);
-    //Get a random integer between the min and max value.
-    var randIndex = Math.floor(Math.random() * (max - min)) + min;
-    //Return random value.
-    return arr[randIndex];
+GameInfo = {
+	currentPlayers: 0
 }
 
-connectedCount = 0;
-var allClients = [];
+UpdateInfo = () => {
+	$("#playersPlayingText").text(`${GameInfo.currentPlayers} Pessoas Jogando`);
+};
 
-var temas = ["carro","melancia"];
-
-var server = app.listen(80,()=>{
-  console.log("running on localhost:80");
+socket.on("RequestRoomAccept",(data) => {
+	socket.emit("Request_RoomInfo",data);
+	setTimeout(()=> {
+		window.location.href = window.location.origin + "/draw/";
+	},1000);
 });
 
-var ingame = false;
-var tempToRemove = 0;
-var temp = 30;
+socket.on("responseRoomInfo",(data) => {
+	console.log(data);
+	$(`#users_${data.RoomName}`).text(`${data.Users.length}/${data.Max}`);
+});
 
-const io = require("socket.io")(server);
+socket.on("tick",(data) => {
+	GameInfo.currentPlayers = data.Users;
+	socket.emit("Request_RoomInfo","Alimentos");
+	UpdateInfo();
+});
 
-app.use(express.static('public'));
-
-io.on("connection",(socket)=> {
-
-  connectedCount +=1;
-	 allClients.push(socket);
-	  io.emit("count",(allClients.length));
-
-  socket.on('chat',(data)=>{
-    io.emit('chat',data);
-  });
-
-  socket.on("game",(data)=>{
-    io.emit('game',data);
-  });
-
-  socket.on("update",(data)=>
-{
-  if(temp>0)
-  {
-    if(tempToRemove<=500){
-    tempToRemove = tempToRemove +1;
-  }else{
-    temp = temp -=1;
-    tempToRemove = 0;
-  }
+$("#alertNick").hide();
+//https://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
 }
-    io.emit("updated",{gameUpdated:true,time:temp});
+
+$("body").ready(()=> {
+	 if(findGetParameter("errornick") != null){
+	 	$("#alertNick").show();
+	 } 
 });
 
-io.on("disconnect",(socket)=>{
-  connectedCount-=1;
-    console.log('Got disconnect!');
-      var i = allClients.indexOf(socket);
-      allClients.splice(i, 1);
-  io.emit("count",(allClients.length));
-});
-
-
-  if(connectedCount>=2 && ingame==false){
-    ingame = true;
-    temp = 30;
-    tempToRemove = 0;
-    io.emit("startgame",{
-      tempo:temp,
-      istrue:ingame,
-      tema:getRandomArrayElement(temas)
-    });
-  }
-});
+ function connectRoom () {
+ 	var getNick = $("#nickNameInput").val();
+ 	if(getNick.length<5){
+ 		window.location = window.location.origin + "?errornick";
+ 	}else{
+		 let getroom = $("#playRoom").attr("room");
+		 console.log(getroom);
+ 		socket.emit("Request_ConnectRoom",{
+ 			room:getroom,
+ 			User:{
+ 				Name: getNick,
+ 				Points: 0,
+ 				ID: socket.id
+ 			}
+ 		});
+ 	}
+ }
